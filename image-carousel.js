2eb5a6ecf9870a90df4148524336493247d41b25
Jorge Gonzalez <39781882+mau628@users.noreply.github.com>
2023-07-29 08:56:32 -0600

    Add comments

--- image-carousel.js
+++ image-carousel.js
@@ -1,9 +1,10 @@
 var currentUrl = window.location.href;
-var btnCarousel = document.querySelectorAll("[data-test='gallery-reel-control-btn-next']");
+var nextButton = document.querySelectorAll("[data-test='gallery-reel-control-btn-next']");
 var buttons = document.querySelectorAll(".gallery-reel-control-btn");
 var clicked = false;
 var autoEventRaised = false;
 
+// Handles click event for previous/next buttons. Disables autoscroll.
 Array.from(buttons).forEach(btn => {
   btn.addEventListener('click', function(event) {
     if(!autoEventRaised) {
@@ -12,14 +13,17 @@ Array.from(buttons).forEach(btn => {
   });
 });
 
+// Handles autoscroll for carousel. Emulates a click.
 function carouselClick() {
   if(!clicked) {
     autoEventRaised = true;
-    btnCarousel[0].click();
+    nextButton[0].click();
     autoEventRaised = false;
   }
 }
 
+// Run the script every 3 seconds.
+// Change example.com with your domain.
 if(currentUrl.includes("example.com")) {
   setInterval(carouselClick, 3000);
 }
